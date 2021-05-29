d2bf7f7f25089d5c07e07b48cbf294fa210b59dd
Adam Moore <adamoore@gmail.com>
2021-05-29 10:49:24 -0400

    product data being displaying to log, styling importe, starting on displaying products.

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -1,7 +1,11 @@
-import '../theme';
-import getAllProducts from '../graphql/collection-starter-code';
-
+import "../theme";
+import getAllProducts from "../graphql/collection-starter-code";
 
 // getAllProducts('test-collection') => returns a Promise, which resolves to an Array of Product Objects
 
 // Your Code Here
+getAllProducts("test-collection").then((arr) =>
+  arr.forEach((element) => {
+    console.log(element);
+  })
+);
