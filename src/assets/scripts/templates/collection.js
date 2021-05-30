828146b66538a08a6ef991414d6c24d944b85aad
Adam Moore <adamoore@gmail.com>
2021-05-30 13:41:35 -0400

    somewhat working example with radio selectors

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -4,8 +4,22 @@ import getAllProducts from "../graphql/collection-starter-code";
 // getAllProducts('test-collection') => returns a Promise, which resolves to an Array of Product Objects
 
 // Your Code Here
+// use below when done
+// getAllProducts("test-collection");
+
 getAllProducts("test-collection").then((arr) =>
   arr.forEach((element) => {
-    console.log(element, element.options[0].values);
+    console.log(element);
   })
 );
+
+// getting a NodeList of all the radio buttons
+const input = document.querySelectorAll(".swatch");
+
+// for each of the nodes in the NodeList they are to call the function handleInput when they are selected.
+input.forEach((e) => (e.onchange = handleInput));
+
+// right now just logging the target data when a radio is selected.
+function handleInput(e) {
+  console.log(e.target);
+}
